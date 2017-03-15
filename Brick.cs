9f29d1ec9083abddff5d43935a89fdba3193a6b6
jmerten323 <mertenj1@hawkmail.newpaltz.edu>
2017-03-14 20:29:12 -0400

    Brick Script

---
+++ Brick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public int hitsToKill;
+    private int numberOfHits;
+    // Use this for initialization
+    void Start()
+    {
+        numberOfHits = 0;
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ball") ;
+        {
+            numberOfHits++;
+            if (numberOfHits == hitsToKill)
+            {
+                Destroy(this.gameObject);
+                Debug.Log(hitsToKill);
+                FindObjectOfType<Ball>().YouBrokeABrick();
+            }
+        }
+    }
+
+}
+
